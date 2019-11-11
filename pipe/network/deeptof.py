b76f93007aea957d0135c6cabfaffa14e70e906f
dong1015323606 <1015323606@qq.com>
2019-11-11 21:11:17 +0800

    将原来在models.py中的网络结构文件拆分为多个以网络命名的.py文件

---
+++ pipe/network/deeptof.py
@@ -0,0 +1,220 @@
+import sys
+
+sys.path.insert(0, '../module/')
+import tensorflow as tf
+from activation import *
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+PI = 3.14159265358979323846
+flg = False
+dtype = tf.float32
+
+def deeptof(x, flg, regular, batch_size, deformable_range, z_multiplier):
+    """Build a U-Net architecture"""
+
+    """ Args: x is the input, 4-D tensor (BxHxWxC)
+              flg represent weather add the BN
+              regular represent the regularizer number 
+
+
+        Return: output is 4-D Tensor (BxHxWxC)
+    """
+
+    pref = 'unet_'
+
+    # whether to train flag
+    train_ae = flg
+
+    # define initializer for the network
+    keys = ['conv', 'upsample']
+    keys_avoid = ['OptimizeLoss']
+    inits = []
+
+    init_net = None
+    if init_net != None:
+        for name in init_net.get_variable_names():
+            # select certain variables
+            flag_init = False
+            for key in keys:
+                if key in name:
+                    flag_init = True
+            for key in keys_avoid:
+                if key in name:
+                    flag_init = False
+            if flag_init:
+                name_f = name.replace('/', '_')
+                num = str(init_net.get_variable_value(name).tolist())
+                # self define the initializer function
+                from tensorflow.python.framework import dtypes
+                from tensorflow.python.ops.init_ops import Initializer
+                exec(
+                    "class " + name_f + "(Initializer):\n def __init__(self,dtype=tf.float32): self.dtype=dtype \n def __call__(self,shape,dtype=None,partition_info=None): return tf.cast(np.array(" + num + "),dtype=self.dtype)\n def get_config(self):return {\"dtype\": self.dtype.name}")
+                inits.append(name_f)
+
+    # autoencoder
+    n_filters = [
+        16, 16,
+        16, 16,
+        16, 16,
+        32, 32,
+        32, 32,
+        32, 32,
+        32
+    ]
+    filter_sizes = [
+        5, 5,
+        5, 5,
+        5, 5,
+        5, 5,
+        5, 5,
+        5, 5,
+        5
+    ]
+    pool_sizes = [ \
+        1, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2
+    ]
+    pool_strides = [
+        1, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2, 1,
+        2
+    ]
+    skips = [ \
+        False, True,
+        False, True,
+        False, True,
+        False, True,
+        False, True,
+        False, True,
+        False
+    ]
+
+    # change space
+    ae_inputs = tf.identity(x, name='ae_inputs')
+
+    # prepare input
+    current_input = tf.identity(ae_inputs, name="input")
+    # convolutional layers: encoder
+    conv = []
+    pool = [current_input]
+    for i in range(1, len(n_filters)):
+        name = pref + "conv_" + str(i)
+
+        # define the initializer
+        if name + '_bias' in inits:
+            bias_init = eval(name + '_bias()')
+        else:
+            bias_init = tf.zeros_initializer()
+        if name + '_kernel' in inits:
+            kernel_init = eval(name + '_kernel()')
+        else:
+            kernel_init = None
+
+        # convolution
+        conv.append( \
+            tf.layers.conv2d( \
+                inputs=current_input,
+                filters=n_filters[i],
+                kernel_size=[filter_sizes[i], filter_sizes[i]],
+                padding="same",
+                activation=leaky_relu,
+                trainable=train_ae,
+                kernel_initializer=kernel_init,
+                bias_initializer=bias_init,
+                name=name,
+            )
+        )
+        # conv[-1] = tf.layers.batch_normalization(
+        #     inputs = conv[-1],
+        #     training = train_ae,
+        #     name = pref + "BN_" + str(i))
+        if pool_sizes[i] == 1 and pool_strides[i] == 1:
+            pool.append(conv[-1])
+        else:
+            pool.append( \
+                tf.layers.max_pooling2d( \
+                    inputs=conv[-1],
+                    pool_size=[pool_sizes[i], pool_sizes[i]],
+                    strides=pool_strides[i],
+                    name=pref + "pool_" + str(i)
+                )
+            )
+        current_input = pool[-1]
+
+    # convolutional layer: decoder
+    # upsampling
+    upsamp = []
+    current_input = pool[-1]
+    for i in range(len(n_filters) - 1, 0, -1):
+        name = pref + "upsample_" + str(i - 1)
+
+        # define the initializer
+        if name + '_bias' in inits:
+            bias_init = eval(name + '_bias()')
+        else:
+            bias_init = tf.zeros_initializer()
+        if name + '_kernel' in inits:
+            kernel_init = eval(name + '_kernel()')
+        else:
+            kernel_init = None
+
+        # upsampling
+        current_input = tf.layers.conv2d_transpose( \
+            inputs=current_input,
+            filters=n_filters[i - 1],
+            kernel_size=[filter_sizes[i], filter_sizes[i]],
+            strides=(pool_strides[i], pool_strides[i]),
+            padding="same",
+            activation=leaky_relu,
+            trainable=train_ae,
+            kernel_initializer=kernel_init,
+            bias_initializer=bias_init,
+            name=name
+        )
+        # current_input = tf.layers.batch_normalization(
+        #     inputs=current_input,
+        #     training=train_ae,
+        #     name=pref + "upsamp_BN_" + str(i))
+        # skip connection
+        if skips[i - 1] == True:
+            current_input = current_input + pool[i - 1]
+        upsamp.append(current_input)
+
+    ###conv out
+    name = pref + "convout_" + str(i - 1)
+
+    # define the initializer
+    if name + '_bias' in inits:
+        bias_init = eval(name + '_bias()')
+    else:
+        bias_init = tf.zeros_initializer()
+    if name + '_kernel' in inits:
+        kernel_init = eval(name + '_kernel()')
+    else:
+        kernel_init = None
+    current_input = tf.layers.conv2d( \
+        inputs=upsamp[-1],
+        filters=1,
+        kernel_size=[5, 5],
+        strides=(1, 1),
+        padding="same",
+        activation=leaky_relu,
+        trainable=train_ae,
+        kernel_initializer=kernel_init,
+        bias_initializer=bias_init,
+        name=name,
+    )
+
+    # current_input = tf.squeeze(current_input, [3])
+    output = tf.identity(current_input, name="ae_output")
+    return output
---
+++ pipe/network/kpn_raw.py
@@ -0,0 +1,48 @@
+import sys
+
+sys.path.insert(0, '../module/')
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+PI = 3.14159265358979323846
+flg = False
+dtype = tf.float32
+
+def kpn_subnet_raw(x, flg, regular, batch_size, deformable_range):
+    output = kpn(x, flg, regular)
+    biass = output[:, :, :, -9::]
+    kers = output[:, :, :, 0:-9]
+    kers = tf.reshape(kers, [-1, tf.shape(x)[1], tf.shape(x)[2], 1 * 1 * 9, 9])
+
+    #
+    x_new = []
+    for i in range(9):
+        ker = kers[:, :, :, :, i]
+        bias = biass[:, :, :, i]
+        x_new.append(tf.reduce_sum(ker * x, -1))
+    x_new = tf.stack(x_new, -1)
+
+    return x_new
+
+def kpn_raw(x, flg, regular, batch_size, deformable_range):
+    # inputs 9 channel raw measurements, float32
+    # outputs 9 channel raw measurements, float32
+    x_shape = [-1, 384, 512, 9]
+    y_shape = [-1, 384, 512, 9]
+
+    output = kpn_subnet_raw(x, flg)
+    biass = output[:,:,:,-9::]
+    kers = output[:,:,:,0:-9]
+    kers = tf.reshape(kers,[-1, tf.shape(x)[1], tf.shape(x)[2], 1*1*9, 9])
+
+    #
+    x_new = []
+    for i in range(9):
+        ker = kers[:,:,:,:,i]
+        bias = biass[:,:,:,i]
+        # x_new.append(tf.reduce_sum(ker * x,-1)+bias)
+        x_new.append(tf.reduce_sum(ker * x,-1))
+    x_new = tf.stack(x_new, -1)
+
+    return x_new, output
