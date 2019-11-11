f35daf345f45a2de82bfb5a5a96b63eb1c9f71e1
dong1015323606 <1015323606@qq.com>
2019-11-11 20:02:49 +0800

    将loss函数与metric函数分开，并添加了一些loss函数如下： SSIM, SSIM_l1, mean_SSIM, mean_SSIM_l1, ZNCC, cos_similarity, huber, mean_huber, sum_huber

--- pipe/loss.py
+++ pipe/loss.py
@@ -1,36 +1,98 @@
 import sys
 
 sys.path.insert(0, '../sim/')
-import numpy as np
 import tensorflow as tf
-import os, json, glob
-import imageio
-import matplotlib
-import math
-# matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from utils import *
-from tof_class import *
-import pdb
-import pickle
-import time
-import scipy.misc
-from scipy import sparse
-import scipy.interpolate
-from copy import deepcopy
-from joblib import Parallel, delayed
-import multiprocessing
-from kinect_spec import *
-import cv2
-from numpy import linalg as LA
-
-from tensorflow.contrib import learn
-from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 tf.logging.set_verbosity(tf.logging.INFO)
-from kinect_init import *
 
+
+def huber(x, y, c=1.0):
+    diff = x - y
+    l2 = tf.square(diff)
+    l1 = tf.abs(diff)
+    # c = (ratio)*tf.reduce_max(diff)
+    diff = tf.where(tf.greater(diff, c), 0.5 * tf.square(c) + c * (l1 - c), 0.5 * l2)
+    return diff
+
+def mean_huber(x, y, mask=None):
+    """
+    Mean huber loss
+    Args:
+        x: predicted image
+        y: target image
+        mask: compute only on this points
+    """
+    if mask is None:
+        mask = tf.ones_like(x, dtype=tf.float32)
+
+    return tf.reduce_mean(huber(x, y) * mask)
+
+def sum_huber(x, y, mask=None):
+    """
+    Sum huber loss
+    Args:
+        x: predicted image
+        y: target image
+        mask: compute only on this points
+    """
+    if mask is None:
+        mask = tf.ones_like(x, dtype=tf.float32)
+
+    return tf.reduce_sum(huber(x, y) * mask)
+
+def ZNCC(x,y):
+	"""
+	ZNCC dissimilarity measure (Zero Mean Normalized Cross-Correlation)
+	Args:
+		x: predicted image
+		y: target image
+	"""
+	mean_x = tf.reduce_mean(x)
+	mean_y = tf.reduce_mean(y)
+	norm_x = x-mean_x
+	norm_y = y-mean_y
+	variance_x = tf.sqrt(tf.reduce_sum(tf.square(norm_x)))
+	variance_y = tf.sqrt(tf.reduce_sum(tf.square(norm_y)))
+
+	zncc = tf.reduce_sum(norm_x*norm_y)/(variance_x*variance_y)
+	return 1-zncc
+
+def SSIM(x, y):
+    """
+    SSIM dissimilarity measure
+    Args:
+        x: predicted image
+        y: target image
+    """
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+    mu_x = tf.nn.avg_pool(x, [1, 3, 3, 1], [1, 1, 1, 1], padding='VALID')
+    mu_y = tf.nn.avg_pool(y, [1, 3, 3, 1], [1, 1, 1, 1], padding='VALID')
+
+    sigma_x = tf.nn.avg_pool(x ** 2, [1, 3, 3, 1], [1, 1, 1, 1], padding='VALID') - mu_x ** 2
+    sigma_y = tf.nn.avg_pool(y ** 2, [1, 3, 3, 1], [1, 1, 1, 1], padding='VALID') - mu_y ** 2
+    sigma_xy = tf.nn.avg_pool(x * y, [1, 3, 3, 1], [1, 1, 1, 1], padding='VALID') - mu_x * mu_y
+
+    SSIM_n = (2 * mu_x * mu_y + C1) * (2 * sigma_xy + C2)
+    SSIM_d = (mu_x ** 2 + mu_y ** 2 + C1) * (sigma_x + sigma_y + C2)
+
+    SSIM = SSIM_n / SSIM_d
+
+    return tf.clip_by_value((1 - SSIM) / 2, 0, 1)
+
+def SSIM_l1(x,y,alpha=0.85):
+	ss = tf.pad(SSIM(x,y),[[0,0],[1,1],[1,1],[0,0]])
+	ll = l1(x,y)
+	return alpha*ss+(1-alpha)*ll
+
+def mean_SSIM(x,y):
+	"""
+	Mean error over SSIM reconstruction
+	"""
+	return tf.reduce_mean(SSIM(x,y))
+
+def mean_SSIM_l1(x, y):
+	return 0.4* mean_SSIM(x, y) + 0.6 * mean_l1(x, y)
 
 def smoothness(x, y):
     """
@@ -72,7 +134,6 @@ def smoothness(x, y):
 
     return tf.reduce_mean(smoothness_x + smoothness_y)
 
-
 def mean_l1(x, y, mask=None):
     """
     Mean reconstruction error
@@ -85,8 +146,7 @@ def mean_l1(x, y, mask=None):
         mask = tf.ones_like(x, dtype=tf.float32)
     return tf.reduce_sum(mask * tf.abs(x - y)) / tf.reduce_sum(mask)
 
-
-def mean_l2(x, y, mask):
+def mean_l2(x, y, mask=None):
     """
     Mean squarred error
     Args:
@@ -97,7 +157,6 @@ def mean_l2(x, y, mask):
     if mask is None:
         mask = tf.ones_like(x, dtype=tf.float32)
     return tf.reduce_sum(mask*tf.square(x - y)) / tf.reduce_sum(mask)
-
 
 def sum_l1(x, y, mask=None):
     """
@@ -111,7 +170,6 @@ def sum_l1(x, y, mask=None):
         mask = tf.ones_like(x, dtype=tf.float32)
     return tf.reduce_sum(mask * tf.abs(x - y))
 
-
 def sum_l2(x, y, mask=None):
     """
     Sum squarred error
@@ -124,6 +182,21 @@ def sum_l2(x, y, mask=None):
         mask = tf.ones_like(x, dtype=tf.float32)
     return tf.reduce_sum(mask * tf.square(x - y))
 
+def sign_and_elementwise(x,y):
+	"""
+	Return the elementwise and of the sign between vectors
+	"""
+	element_wise_sign = tf.sigmoid(10*(tf.sign(x)*tf.sign(y)))
+	return tf.reduce_mean(tf.sigmoid(element_wise_sign))
+
+def cos_similarity(x,y,normalize=False):
+	"""
+	Return the cosine similarity between (normalized) vectors
+	"""
+	if normalize:
+		x = tf.nn.l2_normalize(x)
+		y = tf.nn.l2_normalize(y)
+	return tf.reduce_sum(x*y)
 
 SUPERVISED_LOSS = {
     'mean_l1': mean_l1,
@@ -131,6 +204,15 @@ SUPERVISED_LOSS = {
     'mean_l2': mean_l2,
     'sum_l2': sum_l2,
     'smoothness': smoothness,
+    'SSIM':SSIM,
+	'SSIM_l1':SSIM_l1,
+    'mean_SSIM':mean_SSIM,
+	'mean_SSIM_l1':mean_SSIM_l1,
+	'ZNCC':ZNCC,
+	'cos_similarity':cos_similarity,
+    'huber':huber,
+    'mean_huber':mean_huber,
+	'sum_huber':sum_huber
 }
 
 ALL_LOSSES = dict(SUPERVISED_LOSS)
@@ -141,39 +223,3 @@ def get_supervised_loss(name, x, y, mask=None):
         raise Exception('Unknown loss function selected')
     base_loss_function = ALL_LOSSES[name]
     return base_loss_function(x, y, mask)
-
-def get_mae(depth_outs, gt, msk):
-    return tf.reduce_sum(msk * tf.abs(depth_outs - gt)) / tf.reduce_sum(msk)
-
-def get_metrics_psnr(depth, ori_depth, gt, depth_msk, ori_msk, gt_msk):
-    ori_mse, update_op_ori = tf.metrics.mean_squared_error(gt, ori_depth)
-    ori_psnr = 10 * (tf.log(25.0 / ori_mse) / tf.log(10.0))
-    update_op_ori = 10 * (tf.log(25.0 / update_op_ori) / tf.log(10.0))
-    pre_mse, update_op_pre = tf.metrics.mean_squared_error(gt, depth)
-    pre_psnr = 10 * (tf.log(25.0 / pre_mse) / tf.log(10.0))
-    update_op_pre = 10 * (tf.log(25.0 / update_op_pre) / tf.log(10.0))
-
-    depth = depth * gt_msk
-    ori_depth = ori_depth * gt_msk
-    ori_mse_dm, update_op_ori_dm = tf.metrics.mean_squared_error(gt, ori_depth)
-    ori_psnr_dm = 10 * (tf.log(25.0 / ori_mse_dm) / tf.log(10.0))
-    update_op_ori_dm = 10 * (tf.log(25.0 / update_op_ori_dm) / tf.log(10.0))
-    # pre_mse_dm, update_op_pre_dm = tf.metrics.mean_squared_error(gt * depth_msk, depth)
-    pre_mse_dm, update_op_pre_dm = tf.metrics.mean_squared_error(gt, depth)
-    pre_psnr_dm = 10 * (tf.log(25.0 / pre_mse_dm) / tf.log(10.0))
-    update_op_pre_dm = 10 * (tf.log(25.0 / update_op_pre_dm) / tf.log(10.0))
-
-    return (ori_psnr, update_op_ori), (pre_psnr, update_op_pre), \
-           (ori_psnr_dm, update_op_ori_dm), (pre_psnr_dm, update_op_pre_dm)
-
-def get_metrics_mae(depth, ori_depth, gt, depth_msk, ori_msk, gt_msk):
-    ori_mae, update_ori_mae = tf.metrics.mean_absolute_error(gt, ori_depth)
-    pre_mae, update_pre_mae = tf.metrics.mean_absolute_error(gt, depth)
-
-    gt_msk_k = tf.reduce_mean(gt_msk)
-    depth = depth * gt_msk
-    ori_depth = ori_depth * gt_msk
-    ori_mae_dm, update_ori_mae_dm = tf.metrics.mean_absolute_error(gt / gt_msk_k, ori_depth / gt_msk_k)
-    pre_mae_dm, update_pre_mae_dm = tf.metrics.mean_absolute_error(gt / gt_msk_k, depth / gt_msk_k)
-    return (ori_mae, update_ori_mae), (pre_mae, update_pre_mae), \
-           (ori_mae_dm, update_ori_mae_dm), (pre_mae_dm, update_pre_mae_dm)
