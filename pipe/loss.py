a26eb18facfeca003a23ca9842c88951abe9819e
dong1015323606 <1015323606@qq.com>
2019-11-14 21:41:47 +0800

    add sobel gradient loss

--- pipe/loss.py
+++ pipe/loss.py
@@ -221,6 +221,27 @@ def cos_similarity(x,y,normalize=False):
 		y = tf.nn.l2_normalize(y)
 	return tf.reduce_sum(x*y)
 
+def sobel_edges(img):
+    ch = img.get_shape().as_list()[3]
+    kerx = tf.constant([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype='float32')
+    kery = tf.constant([[-1, -2, 1], [0, 0, 0], [1, 2, 1]], dtype='float32')
+    kerx = tf.expand_dims(kerx, 2)
+    kerx = tf.expand_dims(kerx, 3)
+    kerx = tf.tile(kerx, [1, 1, ch, 1])
+    kery = tf.expand_dims(kery, 2)
+    kery = tf.expand_dims(kery, 3)
+    kery = tf.tile(kery, [1, 1, ch, 1])
+    gx = tf.nn.depthwise_conv2d_native(img, kerx, strides=[1,1,1,1], padding="VALID")
+    gy = tf.nn.depthwise_conv2d_native(img, kery, strides=[1,1,1,1], padding="VALID")
+    return tf.concat([gx, gy], 3)
+
+
+def sobel_gradient_loss(x, y):
+    g1 = sobel_edges(x)
+    g2 = sobel_edges(y)
+    return tf.reduce_mean(tf.pow(tf.abs(g1 - g2),1))
+
+
 SUPERVISED_LOSS = {
     'mean_l1': mean_l1,
     'sum_l1': sum_l1,
@@ -235,7 +256,8 @@ SUPERVISED_LOSS = {
 	'cos_similarity':cos_similarity,
     'huber':huber,
     'mean_huber':mean_huber,
-	'sum_huber':sum_huber
+	'sum_huber':sum_huber,
+    'sobel_gradient': sobel_gradient_loss
 }
 
 ALL_LOSSES = dict(SUPERVISED_LOSS)
